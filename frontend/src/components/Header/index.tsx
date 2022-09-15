d2cd8dce10b521b1cff0a499cff0de655bd79486
André Paterno Hilário <80863194+AndrePaternoHilario@users.noreply.github.com>
2022-09-14 22:17:37 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,16 +1,20 @@
-import logo from '../../assets/img/logo.svg'
+import logo from '../../assets/img/logo.svg';
 
-import './styles.css'
+import './styles.css';
+
 function Header() {
     return (
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="" />
-                    <h1>DSMeta</h1>
-                    <p>Desenvolvido por <a href="https://www.instagram.com/paternohilario/">@paternohilario</a></p>
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/paternohilario/">@paternohilario</a>
+                </p>
             </div>
         </header>
     )
 }
 
-export default Header
+export default Header;
